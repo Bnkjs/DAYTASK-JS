27e0d5cfa1704ff94e68667c413ad5787d428740
Boris <seikenfr@yahoo.fr>
2021-08-30 12:39:45 +0200

    modified: form component

--- src/component/card/cardTask.jsx
+++ src/component/card/cardTask.jsx
@@ -1,9 +1,18 @@
+import {useState } from 'react';
+
 export const CardTask=({category,title,desc})=>{
+  const [showTask, setShowTask] = useState(false);
+
   return(<>
-  <div id="task-container">
-    <div className="task-header">{category}</div>
-    <div className="task-content">{title}</div>
-    <div className="task-validate">{desc}</div>
-  </div>
+          <div className="header-btn">
+           <button onClick={()=>{setShowTask(!showTask)}} >^</button>
+          </div>
+    {showTask &&
+      <div id="task-container">
+        <div className="task-header">{category}</div>
+        <div className="task-content">{title}</div>
+        <div className="task-validate">{desc}</div>
+      </div>
+    }
   </>)
 }
--- src/component/card/cardTaskContainer.jsx
+++ src/component/card/cardTaskContainer.jsx
@@ -1,34 +1,49 @@
-import react,{ useState } from 'react';
-import { CardTask } from './cardTask';
+import react,{ useState, useEffect } from 'react';
 import './card.scss';
 import { FormTask } from '../form/form';
 import { v4 as uuidv4 } from 'uuid';
+import { TaskHeader } from '../card/cardHeader';
+import { CardTask } from '../card/cardTask';
 
 export const CardTaskContainer=()=>{
+  const [category, setCategory]=useState('');
+  const onChangeCategory = event => setCategory(event.target.value);
+  const [title, setTitle] = useState('');
+  const onChangeTitle= event => setTitle(event.target.value);
+  const [description, setDescription] = useState('');
+  const onChangeDescription = event => setDescription(event.target.value);
+  const [taskList, setTaskList] = useState([])
+  const newTaskList = {
+    category: category,
+    title: title,
+    description:description
+  }
+   
+ const handleSubmit=()=>{
+  setTaskList(taskList => taskList.concat(newTaskList))
+  }
 
-  const [showTask, setShowTask] = useState(false);
-  const itemsParsed = JSON.parse(localStorage.getItem('list-items'));
+  useEffect(()=>{
+    console.log(taskList);
+  })
   
-  const listItems = itemsParsed.map((items) => 
-   <CardTask key={uuidv4().toString()} category={items.category} title={items.title} desc={items.description} />
-  )
 
   return(
   <div id='container-card-task'>
-    <FormTask/>
+    <FormTask key={uuidv4().toString()} category={category} onChangeCategory={onChangeCategory} title={title} onChangeTitle={onChangeTitle} description={description} onChangeDescription={onChangeDescription} handleFunction={handleSubmit} />
      <header>
-      <h1>Salut</h1>
+      <h1 >Salut</h1>
       <p>Quelle mission veux-tu ajouter?</p>
      </header>
-      <div className='card-task-header'>
-        <div className="header-btn">
-          <button onClick={()=>{setShowTask(!showTask)}} >^</button>
-        </div>
-      </div>
-      {
-        showTask && <div className="task-content">
-          {listItems}
-      </div>
+     {
+        taskList.map((items)=>{
+          return(<>
+            <TaskHeader key={uuidv4().toString()} category={items.category}/> 
+            <div className="task-content">
+              <CardTask key={uuidv4().toString()} category={items.category} title={items.title} desc={items.description}/>
+            </div>
+          </>) 
+        })
       }
   </div>)
 }
